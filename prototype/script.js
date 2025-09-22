f8cc5ca33e828a08106806358fd6d9244b0f6a59
ahmed <ahmedelmorabiti123460@gmail.com>
2025-09-22 11:47:27 +0100

    proto2

--- prototype/script.js
+++ prototype/script.js
@@ -36,11 +36,28 @@ for (let i = 0; i < arr.length; i++) {
     }
 }
 
-console.log("C1: " + cC1);
-console.log("C2: " + cC2);
-console.log("C3: " + cC3);
-console.log("C4: " + cC4);
-console.log("C5: " + cC5);
-console.log("C6: " + cC6);
-console.log("C7: " + cC7);
-console.log("C8: " + cC8);
+let answer = prompt("choose from C1, C2, C3, C4, C5, C6, C7 or C8");
+if (answer === "C1") {
+    alert("you chose C1. there are " + cC1 + " C1s in the array");
+}
+else if (answer === "C2") {
+    alert("you chose C2. there are " + cC2 + " C2s in the array");
+}
+else if (answer === "C3") {
+    alert("you chose C3. there are " + cC3 + " C3s in the array");
+}
+else if (answer === "C4") {
+    alert("you chose C4. there are " + cC4 + " C4s in the array");
+}
+else if (answer === "C5") {
+    alert("you chose C5. there are " + cC5 + " C5s in the array");
+}
+else if (answer === "C6") {
+    alert("you chose C6. there are " + cC6 + " C6s in the array");
+}
+else if (answer === "C7") {
+    alert("you chose C7. there are " + cC7 + " C7s in the array");
+}
+else if (answer === "C8") {
+    alert("you chose C8. there are " + cC8 + " C8s in the array");
+}
